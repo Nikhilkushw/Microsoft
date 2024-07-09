0ef11ac7265803a6f3eb6074da114c34f47273a7
Nikhil Repository <nikhilkush037@gmail.com>
2024-07-09 19:50:35 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default {
-  base: '/project/', // Adjust this to match your deployment path
-}
+export default defineConfig({
+  plugins: [react()],
+  base: '/project/',
+})
